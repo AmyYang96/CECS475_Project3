314b12a0249eaaef95f5f9373cebfff84242b8b1
Amy Yang <hpfanamy@gmail.com>
2018-05-01 09:53:24 -0700

    ai works

--- src/Cecs475.BoardGames.ComputerOpponent/MinimaxAi.cs
+++ src/Cecs475.BoardGames.ComputerOpponent/MinimaxAi.cs
@@ -9,7 +9,7 @@ namespace Cecs475.BoardGames.ComputerOpponent
 {
     internal struct MinimaxBestMove
     {
-        public int Weight { get; set; }
+        public long Weight { get; set; }
         public IGameMove Move { get; set; }
     }
 
@@ -24,20 +24,46 @@ namespace Cecs475.BoardGames.ComputerOpponent
         public IGameMove FindBestMove(IGameBoard b)
         {
             return FindBestMove(b,
-                true ? int.MinValue : int.MaxValue,
-                true ? int.MaxValue : int.MinValue,
+                //true ? int.MinValue : int.MaxValue,
+                //true ? int.MaxValue : int.MinValue,
                 mMaxDepth).Move;
         }
 
-        private static MinimaxBestMove FindBestMove(IGameBoard b, int alpha, int beta, int depthLeft)
+        private static MinimaxBestMove FindBestMove(IGameBoard b, int depthLeft)
         {
             if (depthLeft==0 || b.IsFinished)
             {
-
+                return new MinimaxBestMove()
+                {
+                    Move = null,
+                    Weight = b.BoardWeight
+                };
             }
+            bool isMaximizing = (b.CurrentPlayer == 1) ? true : false;
+            long bestWeight = (isMaximizing) ? long.MinValue : long.MaxValue;
+            IGameMove bestMove = null;
+
+            foreach(var move in b.GetPossibleMoves())
+            {
+                b.ApplyMove(move);
+                var w = FindBestMove(b, depthLeft - 1);
+                b.UndoLastMove();
+                if(isMaximizing && w.Weight > bestWeight)
+                {
+                    bestWeight = w.Weight;
+                    bestMove = move;
+                } else if(!isMaximizing && w.Weight < bestWeight)
+                {
+                    bestWeight = w.Weight;
+                    bestMove = move;
+                }
+            }
+
+
             return new MinimaxBestMove()
             {
-                Move = null
+                Weight = bestWeight,
+                Move = bestMove
             };
         }
 
