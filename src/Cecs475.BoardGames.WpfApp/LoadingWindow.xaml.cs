70d599b0edbc04a229defe9767f1299be4b33849
Michael Black <Michael.Black01@student.csulb.edu>
2018-05-11 12:43:00 -0700

    added code for web api, but commented out

---
+++ src/Cecs475.BoardGames.WpfApp/LoadingWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using RestSharp;
+
+namespace Cecs475.BoardGames.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for LoadingWindow.xaml
+    /// </summary>
+    public partial class LoadingWindow : Window
+    {
+        public LoadingWindow()
+        {
+            InitializeComponent();
+            /*var client = new RestClient("https://cecs475-boardgames.herokuapp.com/");
+            var request = new RestRequest("api/games", Method.GET);
+            var task = client.ExecuteTaskAsync(request);
+            var response = await task;
+
+            if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                MessageBox.Show("No games found");
+            } else
+            {
+
+            }*/
+        }
+    }
+}
