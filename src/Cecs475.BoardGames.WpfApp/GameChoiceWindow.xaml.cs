e9720504540898303d673d5fc52ceb4c7646d881
Michael Black <Michael.Black01@student.csulb.edu>
2018-05-11 14:54:00 -0700

    centered xaml

--- src/Cecs475.BoardGames.WpfApp/GameChoiceWindow.xaml.cs
+++ src/Cecs475.BoardGames.WpfApp/GameChoiceWindow.xaml.cs
@@ -31,9 +31,9 @@ namespace Cecs475.BoardGames.WpfApp {
 
                 foreach (string currentFile in dllFiles)
                 {
-                    //string loadSemantics = System.IO.Path.GetFileNameWithoutExtension(currentFile) + ", Version=1.0.0.0, Culture=neutral, PublicKeyToken=68e71c13048d452a";
+                    string loadSemantics = System.IO.Path.GetFileNameWithoutExtension(currentFile) + ", Version=1.0.0.0, Culture=neutral, PublicKeyToken=68e71c13048d452a";
                     //MessageBox.Show(System.IO.Path.GetFileNameWithoutExtension(currentFile));
-                   // Assembly.Load(loadSemantics);
+                    //Assembly.Load(loadSemantics);
                    
                     Assembly.LoadFrom(currentFile);
                 }
